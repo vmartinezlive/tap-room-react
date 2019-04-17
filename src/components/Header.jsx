3d77da289075cf12c1394e84b27609b81eedb1bd
Victoria & Isaac & Dino <vmartinez@live.com>
2019-04-16 19:07:39 -0700

    add css on header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,30 +8,46 @@ function Header(){
   return (
     <div>
       <style jsx>{`
-        div {
-          display: flex;
-          flex-flow: row wrap;
-          // justify-content: center;
-          border: 1px solid red;
-          background-color
-        }
         h1{
-          width: 100%;
           font:100px/.5em monoton;
           color: #FCFD46;
           text-transform:uppercase;
         }
-        .nightMarket {
+        .portland {
           display: flex;
-          flex-direction: column;
+          align-items: center;
+          justify-content: space-between;
+          flex-direction: row;
+          width: 50%;
+
         }
+        .nightMarket {
+          color: #FCFD46;
+          font-family: Allerta;
+          text-transform: uppercase;
+          margin: 0;
+          margin-left: 1.2rem;
+        }
+        h4 {
+          letter-spacing: .2rem;
+          font-size: 2.5rem;
+          margin-bottom: 0;
+        }
+        h5 {
+          margin-top: -2px;
+          font-size: 2.2rem;
+        }
+
+
      `}</style>
       <Links/>
       <Icon/>
-      <h1>Portland</h1>
-      <div className="nightMarket">
-        <h4>Night</h4>
-        <h4>Market</h4>
+      <div className="portland">
+        <h1>Portland</h1>
+        <div className="nightMarket">
+          <h4>Night</h4>
+          <h5>Market</h5>
+        </div>
       </div>
 
     </div>
