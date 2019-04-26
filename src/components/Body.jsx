177570caa520e55fd5463c8e5b3ee3def561bc9d
Student <example@example.com>
2019-04-26 09:06:33 -0700

    change font to go around image

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -8,23 +8,42 @@ function Body(){
   return (
     <div>
       <style jsx>{`
-        h2{
-          position: absolute;
-          left: 50vw;
-          top: 65vh;
-          z-index: -2;
-          width: 20vw;
-          height: auto;
-          border: 1px solid white;
-          color: white;
-        }
+        // h2{
+        //   position: absolute;
+        //   left: 50vw;
+        //   top: 65vh;
+        //   z-index: -2;
+        //   width: 20vw;
+        //   height: auto;
+        //   border: 1px solid white;
+        //   color: white;
+        // }
         img{
           position:absolute;
           z-index: 1;
-          top: 6vh;
-          left: 13vw;
-          width: 70%;
+          top: 15vh;
+          left: 18vw;
+          width: 60%;
         }
+        h2 span {
+          font: 26px Monaco, MonoSpace;
+          height: 200px;
+          position: absolute;
+          width: 20px;
+          left: 47vw;
+          top: 15vh;
+          transform-origin: bottom center;
+          color: white;
+        }
+      .char1 { transform: rotate(325deg); }
+      .char2 { transform: rotate(335deg); }
+      .char3 { transform: rotate(345deg); }
+      .char4 { transform: rotate(355deg); }
+      .char5 { transform: rotate(5deg); }
+      .char6 { transform: rotate(15deg); }
+      .char7 { transform: rotate(25deg); }
+      .char8 { transform: rotate(35deg); }
+
 			`}
 
       </style>
@@ -33,13 +52,13 @@ function Body(){
       <DisplayKegs/>
       <h2>
         <span className="char1">B</span>
-        <span className="char1">E</span>
-        <span className="char1">E</span>
-        <span className="char1">R</span>
-        <span className="char1">G</span>
-        <span className="char1">A</span>
-        <span className="char1">S</span>
-        <span className="char1">M</span>
+        <span className="char2">E</span>
+        <span className="char3">E</span>
+        <span className="char4">R</span>
+        <span className="char5">T</span>
+        <span className="char6">I</span>
+        <span className="char7">M</span>
+        <span className="char8">E</span>
       </h2>
       <img src={logo} alt="logo"></img>
 
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -9,33 +9,15 @@ function Img(){
           width: 100%;
           height: 80vh;
           img{
-            display: block; 
+            display: block;
             z-index: -2;
           }
-          h2{
-            position: absolute;
-            left: 20vw;
-            top: 10vh;
-            z-index: 1;
-            width: 60vw; 
-            height: auto; 
-            color: white;
-            text-transform: uppercase; 
-            font-size: 4rem; 
-            text-align: center; 
-            // -webkit-text-stroke: .22rem black;
-            font-family: 'Vidaloka', serif;
-            letter-spacing: 0.1em;
-          }
+
 			`}
       </style>
       <img src={background} alt="background"></img>
-      <h2> It's all about the BEER!</h2>   
     </div>
   );
 }
 
 export default Img;
-
-
-  
