f2f771aa4fca16a76a56f2e6df77c68e67a256e0
Victoria Martinez <vmartinez72@live.com>
2019-05-01 13:20:05 -0700

    add image above background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 import NewKegControl from './NewKegControl';
 import kegList from '../model/KegData';
 import DisplayKegs from './DisplayKegs';
-
+import logo from '../assets/images/RedLogo.png';
 import background from '../assets/images/woodback.jpg';
 import About from './About';
 import { Switch, Route } from 'react-router-dom';
@@ -32,21 +32,21 @@ class App extends Component{
     return (
       <div>
         <style jsx global>{`
-        // body {
-        //   background-color: #FFF3E2'
-        //   margin: 0;
-        //   padding: 0;
-        // }
         .backgroundImg {
           background-size: block;
           width: 100%;
           height: 40%;
         }
-
-
+        .logo{
+          position:absolute;
+          z-index: 1;
+          top: 15vh;
+          left: 18vw;
+          width: 60%;
+        }
         `}</style>
         <Header/>
-
+        <img className="logo" src={logo} alt="logo"></img>
         <img className="backgroundImg" src={background} alt="background"></img>
         <Navbar/>
         <Switch>
