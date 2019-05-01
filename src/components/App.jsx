b470b769346d98bc8b29f52a6f235c44e5243dcd
Victoria Martinez <vmartinez72@live.com>
2019-05-01 10:54:06 -0700

    fix header styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Image from './Image';
 import Header from './Header';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import NewKegControl from './NewKegControl';
 import kegList from '../model/KegData';
 import DisplayKegs from './DisplayKegs';
+
 import background from '../assets/images/woodback.jpg';
 import About from './About';
 import { Switch, Route } from 'react-router-dom';
@@ -32,15 +32,22 @@ class App extends Component{
     return (
       <div>
         <style jsx global>{`
-        body {
-          background-color: #FFF3E2'
-          margin: 0;
-          padding: 0;
+        // body {
+        //   background-color: #FFF3E2'
+        //   margin: 0;
+        //   padding: 0;
+        // }
+        .backgroundImg {
+          background-size: block;
+          width: 100%;
+          height: 40%;
         }
+
+
         `}</style>
         <Header/>
-        <Image/>
-        <img src={background} alt="background"></img>
+
+        <img className="backgroundImg" src={background} alt="background"></img>
         <Navbar/>
         <Switch>
           <Route exact path ='/' render={() =><DisplayKegs keglist={this.state.kegList} />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,28 @@ function Header(){
       <style jsx>{`
       background-color: black;
       color: white;
-      height: 2rem;
-      padding-top: 1rem;
       text-align: center;
+      .atags {
+        display: flex;
+        justify-content: space-around;
+        margin-right: 8rem;
+        margin-left: 8rem;
+
+        width: 80%;
+        height: 3rem;
+        padding-top: 1rem;
+        padding-left: 2rem;
+        padding-reight:
+      }
+
+
+
       `}
       </style>
-      <a href="#">Call Us Today! 925-961-9160 |</a>
-      <a href="#">info@8bridgesbrewing.com</a>
+      <div className="atags">
+        <a href="#">Call Us Today! 925-961-9160 |</a>
+        <a href="#">info@8bridgesbrewing.com</a>
+      </div>
     </div>
   );
 }
